7f45292d2422c2d8fc1999e55393133798bbdd69
Rafael Gdl <rafael@vipmarketingla.comcls>
2018-10-17 13:29:32 -0700

    sme changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ const Button = (props) => {
 	return(
 	<button 
 		style= {props.style} 
-		className = {props.type=='primary'? 'btn btn-primary' : 'btn btn-secondary'}
+		className = {props.type==='primary'? 'btn btn-primary' : 'btn btn-secondary'}
 		onClick= {props.action} > 
 		{props.title} 
 	</button>)
